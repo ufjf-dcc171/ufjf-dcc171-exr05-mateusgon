21a1ec69e4c254533217f0f9a2175427a9ecd7f2
ice <ice@lab06m27.nrc.ice.ufjf.br>
2017-08-22 22:50:41 -0300

    Versão finalizada

---
+++ src/br/ufjf/dcc171/Aula05Exr5.java
@@ -0,0 +1,15 @@
+package br.ufjf.dcc171;
+
+import javax.swing.JFrame;
+
+public class Aula05Exr5 {
+
+    public static void main(String[] args) {
+        Janela janela = new Janela();
+        janela.setSize(400, 400);
+        janela.setLocationRelativeTo(null);
+        janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        janela.setVisible(true);
+    }
+    
+}
---
+++ src/br/ufjf/dcc171/Janela.java
@@ -0,0 +1,78 @@
+package br.ufjf.dcc171;
+
+import java.awt.FlowLayout;
+import java.awt.HeadlessException;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+
+class Janela extends JFrame {
+    
+        private final JTextField valor;
+        private final JTextField cotacao;
+        private final JCheckBox chkICMS = new JCheckBox("ICMS");
+        private final JCheckBox chkTributo = new JCheckBox("Tributo");
+        private final JLabel etiqueta = new JLabel("Digite o valor do produto em dólar:");
+        private final JLabel etiqueta2 = new JLabel("Cotação do dólar em relação ao real");
+        private final JButton calcula = new JButton("Valor do produto");
+        
+    public Janela() throws HeadlessException {
+        
+        super ("Cálculo de valor do produto");
+        setLayout(new FlowLayout(FlowLayout.LEFT));
+        
+        add(etiqueta);
+        valor = new JTextField(10);
+        add(valor);
+        add(etiqueta2);
+        cotacao = new JTextField(10);
+        add(cotacao);
+        add(chkICMS);
+        add(chkTributo);
+        add(calcula);
+        
+        insereValor novoValor = new insereValor();
+        calcula.addActionListener(novoValor);
+    }
+
+    private class insereValor implements ItemListener, ActionListener {
+
+        @Override
+        public void itemStateChanged(ItemEvent e) {
+            
+            
+          
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            Double valorDolar = Double.parseDouble(valor.getText());
+            Double valorCotacao = Double.parseDouble(cotacao.getText());
+            Double valorFinal = valorDolar * valorCotacao;
+            
+            if (chkTributo.isSelected())
+            {
+                valorFinal = valorFinal + valorFinal * 0.6;
+                if (chkICMS.isSelected())
+                {
+                    valorFinal = valorFinal + valorFinal * 0.18;
+                }
+            }
+            else
+            {
+                valorFinal = valorFinal;
+            }
+            JOptionPane.showMessageDialog(null, "O valor é " + valorFinal, "Valor", JOptionPane.INFORMATION_MESSAGE);
+        }
+
+        
+    }
+    
+}
